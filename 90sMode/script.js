b4b0b03a5e39f5b3cd10e09837dd238283dca4c3
Miguel Closa <jmrclosa@gmail.com>
2024-08-03 10:24:15 -0400

    Added live clock function in script.js

--- 90sMode/script.js
+++ 90sMode/script.js
@@ -3,24 +3,38 @@ var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 
-// Get date
-let currDate = new Date()
+
+function updateCurrentTime(){
+
+    // Get date
+    let currDate = new Date()
 
 // Weekday
-let wd = currDate.getDay()
+    let wd = currDate.getDay()
 
 
 // Month, day
-let month = currDate.getMonth()
-let d = currDate.getDay()
-let y = currDate.getFullYear()
-
-// Time-related (Hours and minutes)
-let h = currDate.getHours()
-let m = currDate.getMinutes()
+    let month = currDate.getMonth()
+    let d = currDate.getDate()
+    let y = currDate.getFullYear()
 
 
 
-var date = document.getElementById('currentDay').innerHTML = `Today is ${weekdays[wd]}, ${months[month]} ${d}, ${y}. ${h}:${m}`
+
+    // Time-related (Hours and minutes)
+    let h = currDate.getHours()
+    let m = currDate.getMinutes()
+    let s = currDate.getSeconds()
+
+    var date = document.getElementById('currentDay').innerHTML = `Today is ${weekdays[wd]}, ${months[month]} ${d}, ${y}. ${h}:${m}:${s.toString().padStart(2,"0")}`
+
+}
+
+updateCurrentTime()
+setInterval(updateCurrentTime, 1000)
+
+
+
+
 
 // weekday.innerHTML = weekdays[wd]
